8c9749e0eb32b2b2582255fde5da11d233a67683
Light123k <abhishekpandey95609@gmail.com>
2022-07-27 23:33:10 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import Banner from './Components/Banner'
 import Movielists from './Components/Movielists';
+import Favourites from './Components/Favourites';
 function App() {
   return (
     <div>
       <Navbar />
-      <Banner />
-      <Movielists />
+      {/* <Banner />
+      <Movielists /> */}
+      <Favourites />
     </div>
+
 
   );
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,7 +4,7 @@ export default class Navbar extends Component {
     render() {
         return (
             <div style={{ display: 'flex', padding: '0.5' }}>
-                <h1>Movie App</h1>
+                <h1 style={{ marginTop: '1.5rem' }}>Movie App</h1>
                 <h2 style={{ marginLeft: '2rem', marginTop: '1.85rem' }} >Favourites</h2>
             </div>
         )
